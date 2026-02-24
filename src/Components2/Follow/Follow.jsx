41a341e3b4c91ccf7d31e398be90aa7914dedebb
Shohrab Khan <shohrab0000@gmail.com>
2026-02-24 14:05:21 +0530

    first commit

--- src/Components2/Follow/Follow.jsx
+++ src/Components2/Follow/Follow.jsx
@@ -1,6 +1,7 @@
 // src/components/AksHeroSection.js
 import React from "react";
 import "./Follow.css";
+// import { TypeAnimation } from "react-type-animation";
 import { TypeAnimation } from "react-type-animation";
 
 const AksHeroSection = () => {
@@ -8,26 +9,17 @@ const AksHeroSection = () => {
     <div className="aks-hero">
       <div className="aks-hero-content">
         <h2>
-          <TypeAnimation
-            sequence={[
-              "Smart Law", // Types
-              1000, // Waits 1s
-              "Smarter Outcomes", // Replaces
-              1500,
-
-              "", // Clears
-              1000, // Clears
-            ]}
-            wrapper="span"
-            cursor={true}
-            repeat={Infinity}
-            // style={{
-            //   fontSize: "24px",
-            //   fontFamily: "monospace",
-            //   display: "inline-block",
-            //   color: "#222",
-            // }}
-          />
+         <TypeAnimation
+  sequence={[
+    "Smart Law",
+    2000,
+    "Smarter Outcomes",
+    2000,
+  ]}
+  wrapper="span"
+  speed={50}
+  repeat={Infinity}
+/>
         </h2>
         <p>
           <a href="#">Contact us</a> to learn more about{" "}
